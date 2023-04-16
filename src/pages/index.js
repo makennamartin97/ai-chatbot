0d5ff2356bc6d212eabd0dcd71878667f5afcc63
makennamartin97 <makennamartin97@yahoo.com>
2023-04-16 18:28:59 -0400

    fixed bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 import TypingAnimation from "../components/TypingAnimation";
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
   const [chatLog, setChatLog] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const messagesEndRef = useRef(null)
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [chatLog]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,7 +39,6 @@ export default function Home() {
     }
     setIsLoading(true);
     axios.post(url, data, {headers:headers}).then((response)=>{
-      console.log('hi')
       console.log(response)
       setChatLog((prev) => [...prev, {type:'bot', message: response.data.choices[0].message.content}])
       setIsLoading(false)
@@ -43,11 +51,11 @@ export default function Home() {
   return (
     <div className='mx-auto bg-[#242424] h-screen w-screen justify-center flex'>
       <img src={"/icon.png"} alt="icon" className='fixed top-0 h-[100px]'/>
-      <div className='box fixed bottom-0 pt-16 w-screen md:w-[570px] pb-16'>
+      <div className='box fixed bottom-0 pt-16 w-screen pb-16'>
         <div>
           <h1 className='text-center text-xl'>Welcome to Kleightonn<span className='text-[#7000FF]'>AI</span></h1>
         </div>
-        <div className="flex-grow p-6 ">
+        <div className="flex-grow p-6 lg:pl-72 lg:pr-72">
           <div className="flex flex-col overflow-y-scroll">
             {
         chatLog.map((message, index) => (
@@ -71,12 +79,15 @@ export default function Home() {
               </div>
             }
           </div>
+          <div ref={messagesEndRef} />
         </div>
+        <form onSubmit={handleSubmit} className="fixed bottom-0 w-screen p-6 bg-white">
+          <div className="flex rounded-lg border border-gray-700 bg-white w-full ml-4 mr-4 md:ml-16 md:mr-16 lg:ml-32 lg:mr-32">  
+        <input type="text" className="flex-grow px-4 py-2 bg-transparent focus:outline-none" placeholder="Type your message..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 font-semibold focus:outline-none">Send</button>
+            </div>
+        </form>
       </div>
-      <form onSubmit={handleSubmit} className='fixed bottom-0 w-full pb-3'>
-        <input type="text" placeholder='Type your question...' value={inputValue} onChange={(e)=>setInputValue(e.target.value)} id="chat" className='w-[230px] xs:w-[300px] sm:w-[500px]'/>
-        <button type="submit"></button>
-      </form>
     </div>
 
   )

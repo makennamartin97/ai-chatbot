c7bf72b3078e46116a08b559408a2ee877c0e851
makennamartin97 <makennamartin97@yahoo.com>
2023-04-16 16:42:40 -0400

    .env file

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
     const url = 'https://api.openai.com/v1/chat/completions';
     const headers = {
       'Content-type':'application/json',
-      'Authorization': `Bearer ${process.env.local.NEXT_PUBLIC_OPENAI_API_KEY}`
+      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
     };
     const data = {
       model:"gpt-3.5-turbo",
